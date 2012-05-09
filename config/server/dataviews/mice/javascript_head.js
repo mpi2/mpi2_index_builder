01663d54dc88d6ab6e7ee4c69e6c2bc5ea170d0b
Richard Easty <re4@sanger.ac.uk>
2012-05-09 16:43:11 +0100

    delete config/server/dataviews

--- config/server/dataviews/mice/javascript_head.js
+++
@@ -1,8 +0,0 @@
-
-// ikmc-mice custom javascript
-
-jQuery("#search_results .mice_qc_details_toggle").live("click", function () {
-  jQuery(this).parent().parent().next("tr.mice_qc_details").toggle();
-  jQuery(this).toggleClass("toggle-open");
-  jQuery(this).toggleClass("toggle-close");
-});
